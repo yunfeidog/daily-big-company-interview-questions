59a4a519f9808d37456c94ae955a54f926f55b3f
yunfeidog <1844025705@qq.com>
2024-05-03 23:35:26 +0800

    修改一些样式与导航栏

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,10 +2,12 @@ import {navbar} from "vuepress-theme-hope";
 
 export const Navbar = navbar([
     {text: "最新面试题", icon: "blog", link: "/blog/"},
-    {text: "大厂面试题", icon: "free", link: "/interviews/"},
+    {text: "大厂面试", icon: "free", link: "/interviews/"},
+    {text: "后端面试题", icon: "blog", link: "/backEnd/"},
     {text: "前端面试题", icon: "blog", link: "/frontEnd/"},
+    {text: "分类", icon: "tag", link: "/category/"},
+    {text: "标签", icon: "tag", link: "/tag/"},
     {text: "实习招聘汇总", icon: "internship", link: "/internship/"},
     {text: "面经建议", icon: "doc", link: "/experience/"},
-    {text: "标签", icon: "tag", link: "/tag/"},
     {text: "时间线", icon: "timeline", link: "/timeline/"},
 ]);
